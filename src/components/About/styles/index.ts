0e2d5b97e385284e5b660b7446e667137388ae67
Felipe Carvalho <felipe_fsc@hotmail.com>
2020-11-10 21:35:18 -0300

    Timeout para transição de top image + ajuste de padding

--- src/components/About/styles/index.ts
+++ src/components/About/styles/index.ts
@@ -3,26 +3,30 @@ import { Colors } from "../../../shared/styles/Colors";
 import { StandardContainer } from "../../../shared/styles/common";
 
 export const AboutContent = styled.div`
-	display: flex;
-	flex-direction: column;
-	margin-bottom: 40px;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 40px;
 `;
 
 export const DescriptionContainer = styled(StandardContainer)`
-	padding: 50px;
-	display: block;
-	text-align: justify;
+  padding: 50px;
+  display: block;
+  text-align: justify;
 
-	& p:first-child {
-		margin-bottom: 8px;
-	}
+  & p:first-child {
+    margin-bottom: 8px;
+  }
+
+  @media screen and (max-width: 750px) {
+    padding: 20px;
+  }
 `;
 
 export const Hello = styled.span`
-	font-weight: 700;
+  font-weight: 700;
 `;
 
 export const TextHighlight = styled.span`
-	color: ${(props) => props.color || Colors.preto};
-	font-weight: 700;
+  color: ${(props) => props.color || Colors.preto};
+  font-weight: 700;
 `;
